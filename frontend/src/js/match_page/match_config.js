0bb134556b079da11d73abd4c77476c5600810b9
Salvatore Iavarone <lorenzoiava11@gmail.com>
2026-05-15 11:39:38 +0200

    READY TO SUBMIT: js/match_page

---
+++ frontend/src/js/match_page/match_config.js
@@ -0,0 +1,14 @@
+/*
+    FILE: match_config.js
+    DESCRIPTION: Costanti di configurazione condivise tra tutti i moduli della partita.
+    AUTHORS: Salvatore Iavarone & Michele Pio Forlani
+*/
+
+/* Numero totale di round per ogni partita */
+export const TOTAL_ROUNDS = 3;
+
+/* Durata di ogni round in secondi */
+export const ROUND_SECONDS = 90;
+
+/* Chiave localStorage per le impostazioni utente */
+export const SETTINGS_KEY = 'codeguessr-settings';
